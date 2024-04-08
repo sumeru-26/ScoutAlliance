60db9572c55b2fc09c4669688c1c6e70920dd6d3
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-04-07 22:09:27 -0700

    added basic socket stuff

--- server.js
+++ server.js
@@ -1,23 +1,39 @@
 import 'dotenv/config'
 
+import { createServer } from 'node:http';
+
 import express from 'express';
 import { engine } from 'express-handlebars';
+import { Server } from 'socket.io';
 
 import rootRoute from './routes/root.js';
 import infoRoute from './routes/info.js';
 import chatRoute from './routes/chat.js';
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.set('view engine', 'hbs');
+app.set('views', './views');
+
 app.engine('hbs', engine({
     extname: 'hbs'
 }));
-app.set('views', './views');
 
 app.use('/', rootRoute);
 app.use('/info', infoRoute);
 app.use('/chat', chatRoute)
 
+
+io.on('connection', (socket) =>{
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+    })
+})
+
 const port = process.env.PORT || 3000;
-app.listen(port);
+server.listen(port, () => {
+    console.log(`server is currently running on port ${port}`);
+});
